4bdcee5bfe4d85bcc2446bd7cdc97b9175716018
dimiboeckaerts <dimi.boeckaerts@gmail.com>
2020-04-18 18:32:48 +0200

    Final update (2)

--- RBP_alignment.jl
+++ RBP_alignment.jl
@@ -107,10 +107,11 @@ matrix = readdlm("/Users/Dimi/Documents/GitHub_Local/BacteriophageHostPrediction
 # --------------------------------------------------
 seqfile = "/Users/Dimi/Documents/GitHub_Local/BacteriophageHostPrediction/RBP_sequences887.fasta"
 sequence_list = file_to_array(seqfile)
-feature_matrix = zeros(length(sequence_list), 14)
-p = Progress(Int64(round((length(sequence_list)*14), digits=0)))
 scoremodel = AffineGapScoreModel(BLOSUM62, gap_open=-10, gap_extend=-1)
-sample_list = [261, 591, 330, 458, 296, 497, 233, 378, 110, 830, 727, 67, 586, 588]
+sample_list = [633, 229, 404, 434, 415, 442, 848, 421, 245, 375, 748, 741, 249, 361, 492, 525, 523, 800, 255, 390, 261, 452, 406, 288, 305, 237, 394, 446, 485, 459, 501, 209, 396, 186, 184, 500, 554, 420, 511, 339, 479, 530, 317, 296, 367, 469, 215, 508, 381, 542, 343, 397, 425, 219, 276, 541, 467, 218, 497, 347, 388, 238, 489, 559, 676, 400, 869, 437, 201, 408, 365, 564, 353, 412, 308, 372, 555, 351, 494, 244, 522, 486, 312, 331, 730, 714, 876, 3, 638, 757, 54, 602, 13, 88, 767, 594, 154, 102, 75, 814, 771, 590, 159, 752, 23, 760, 654, 679, 701, 675, 782, 86, 703, 871, 828, 664, 605, 844, 665, 671, 711, 687, 612, 716, 731, 685, 570, 587, 585, 574, 569, 576, 575, 579, 577, 573, 581, 571, 584, 588, 583, 580, 586, 833, 582, 572, 578]
+feature_matrix = zeros(length(sequence_list), length(sample_list))
+p = Progress(Int64(round((length(sequence_list)*length(sample_list)), digits=0)))
+
 
 for i in 1:length(sequence_list), j in 1:length(sample_list)
     res = pairalign(LocalAlignment(), sequence_list[i], sequence_list[sample_list[j]], scoremodel)
